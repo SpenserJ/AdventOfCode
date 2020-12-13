71f2049387ec0bee08d032ec581ccb2f1d076704
Spenser Jones <hello@spenserj.com>
2020-12-12 19:28:22 -0700

    adds solution for 2020-2.2

--- src/2020/day2/index.ts
+++ src/2020/day2/index.ts
@@ -1,24 +1,37 @@
 import { loadInput } from '../../utils';
 
 interface Entry {
-  validPattern: RegExp;
+  numbers: [number, number];
+  character: string; // TypeScript doesn't have a type for a fixed-length string
   password: string;
 }
 
 const entryMatchRegExp = /^(\d+)-(\d+) (\w): (.*)$/;
-const entries = loadInput(__dirname)
-  .map((line): Entry => {
-    const parts = entryMatchRegExp.exec(line);
-    if (!parts) { throw new Error(`Failed to parse line: "${line}"`); }
+const entries = loadInput(__dirname).map((line): Entry => {
+  const parts = entryMatchRegExp.exec(line);
+  if (!parts) { throw new Error(`Failed to parse line: "${line}"`); }
 
-    const [, min, max, char, password] = parts;
-    const notChar = `[^${char}]*`;
+  const [, num1, num2, character, password] = parts;
+  return {
+    numbers: [parseInt(num1, 10), parseInt(num2, 10)],
+    character,
+    password,
+  };
+});
 
-    return {
-      validPattern: new RegExp(`^${notChar}(${char}${notChar}){${min},${max}}${notChar}$`),
-      password,
-    };
-  })
-  .filter(({ validPattern, password }) => validPattern.test(password));
+const part1Valid = entries.filter(({ numbers: [min, max], character: char, password }) => {
+  const notChar = `[^${char}]*`;
+  const validPattern = new RegExp(`^${notChar}(${char}${notChar}){${min},${max}}${notChar}$`);
+  return validPattern.test(password);
+});
 
-console.log(entries.length);
+const part2Valid = entries.filter(({ numbers, character, password }) => {
+  // We subtract 1 from the index because the policy doesn't understand zero-based indexes
+  const foundChars = numbers.filter((i) => password[i - 1] === character).length;
+  return foundChars === 1;
+});
+
+console.log({
+  part1: part1Valid.length,
+  part2: part2Valid.length,
+});
